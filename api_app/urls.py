db0558073bd008e78a6e8342101c1c8dd2a98f02
ravigadila <ravi@micropyramid.com>
2019-11-18 01:48:43 +0530

    station update, deleted, get

--- api_app/urls.py
+++ api_app/urls.py
@@ -4,4 +4,5 @@ from api_app.views import station
 urlpatterns = [
     url(r'^station/list/$', station.StationListView.as_view(), name="station_list"),
     url(r'^station/create/$', station.StationCreateView.as_view(), name="station_create"),
+    url(r'^station/(?P<pk>\d+)/$', station.StationUpdateView.as_view(), name="station_create"),
 ]
--- api_app/views/station.py
+++ api_app/views/station.py
@@ -1,4 +1,4 @@
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
 
 from api_app.models import Station, ChargePoint
 from api_app.serializers import StationSerializer
@@ -17,3 +17,12 @@ class StationCreateView(CreateAPIView):
     API endpoint for creating charging station
     """
     serializer_class = StationSerializer
+
+
+class StationUpdateView(RetrieveUpdateDestroyAPIView):
+    """
+    API for Update, Delete, Get Station.
+    """
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
+    lookup_field = 'pk'
