34e5478cce40fc4e768be7041c529dac81bd05fe
ravigadila <ravikumar.gadila@gmail.com>
2023-10-29 11:14:29 +0530

    delete project files

--- api_app/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Station, ChargePoint, ConnectorType
-
-admin.site.register(Station)
-admin.site.register(ChargePoint)
-admin.site.register(ConnectorType)
--- api_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from api_app.views import station
-
-urlpatterns = [
-    url(r'^station/list/$', station.StationListView.as_view(), name="station_list"),
-    url(r'^station/create/$', station.StationCreateView.as_view(), name="station_create"),
-    url(r'^station/(?P<pk>\d+)/$', station.StationUpdateView.as_view(), name="station_create"),
-]
